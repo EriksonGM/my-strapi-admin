ff04759557a0258dedebe12214cf61759fea11a4
Erikson Melgarejo <eriksonlove@gmail.com>
2021-04-15 02:04:49 +0100

    Init

---
+++ admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -0,0 +1,36 @@
+/**
+ *
+ * LeftMenuFooter
+ *
+ */
+
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+import Wrapper from './Wrapper';
+
+function LeftMenuFooter({ version }) {
+  // PROJECT_TYPE is an env variable defined in the webpack config
+  // eslint-disable-next-line no-undef
+  const projectType = PROJECT_TYPE;
+
+  return (
+    <Wrapper>
+      <div className="poweredBy">
+        <a key="website" href="https://www.digitalfactory.co.ao" target="_blank" rel="noopener noreferrer">
+          Digital Factory
+        </a>
+        &nbsp;
+        <a href="https://carrinho-sa.com" target="_blank" rel="noopener noreferrer">
+          — Leonor Carrinho
+        </a>
+      </div>
+    </Wrapper>
+  );
+}
+
+LeftMenuFooter.propTypes = {
+  version: PropTypes.string.isRequired,
+};
+
+export default LeftMenuFooter;
